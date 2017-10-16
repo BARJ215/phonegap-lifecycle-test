82ff026072acfa053036f0611acb68c203f98e87
BARJ215 <barj2_15@uni.worc.ac.uk>
2017-10-16 16:03:28 +0100

    foods_db test

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,29 @@
+var db = window.openDatabase("foods_db", 1.0, "food database", 1024 * 1024);
+db.transaction(createTable, error, success);
+	
+function createTable(tx) {	
+ 	tx.executeSql("CREATE TABLE IF NOT EXISTS foods_db(id UNIQUE, title TEXT)");
+}
+	
+function success() {
+	db.transaction(addFood, error);
+}
+	
+function addFood(tx) {
+	tx.executeSql("INSERT INTO foods_db(id, title) VALUES (1, 'Dinky Decker')");
+	tx.executeSql("INSERT INTO foods_db(id, title) VALUES (2, '130kcal')");
+	tx.executeSql("INSERT INTO foods_db(id, title) VALUES (3, '4.8g')");
+    tx.executeSql("INSERT INTO foods_db(id, title) VALUES (4, '2.9g')");
+    tx.executeSql("INSERT INTO foods_db(id, title) VALUES (5, '15g')");
+    tx.executeSql("INSERT INTO foods_db(id, title) VALUES (6, '0.07g')");
+}
+	
+function error(err) {
+	alert("oops something went wrong " + err.message);
+}
+
+
+///////////////////////////////////////////////////////////////////
 var key = "chocbar";
 var data =[ 
     {"name":"Dinky Decker","calories":"130kcal","fat":"4.8g","saturates":"2.9g","sugars":"15g","salt":"0.07g"}, {"name":"Oreo","calories":"83kcal","fat":"5.2g","saturates":"2.9g","sugars":"7.3g","salt":"0.06g"},
